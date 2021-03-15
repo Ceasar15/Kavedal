8d5ebd480687f7d97526a7bfda28c1eecbd14a31
Ceasar15 <ceasarkwadwo@gmail.com>
2021-03-15 18:47:25 +0000

    check static files rendering

--- Kavedal/settings.py
+++ Kavedal/settings.py
@@ -108,8 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
