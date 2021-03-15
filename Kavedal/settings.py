86dcde5ed57ca9a97a946a5ad80c599fd26a4a55
Ceasar15 <ceasarkwadwo@gmail.com>
2021-03-15 19:12:12 +0000

    added postgresql

--- Kavedal/settings.py
+++ Kavedal/settings.py
@@ -64,10 +64,21 @@ WSGI_APPLICATION = 'Kavedal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Kavedal',
+        'USER': 'bookmarks',
+        'PASSWORD': 'edem1234',
+        'HOST': 'localhost',
+        'PORT': '', 
     }
 }
 
