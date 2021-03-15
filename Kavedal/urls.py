e6ead963ad9e02a22a7f1072ce5cf5f3e67ec736
Ceasar15 <ceasarkwadwo@gmail.com>
2021-03-15 17:43:36 +0000

    added static path

--- Kavedal/urls.py
+++ Kavedal/urls.py
@@ -2,12 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from core import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', include('core.urls', namespace='home')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-]
+if settings.DEBUG: 
