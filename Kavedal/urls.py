0ca078a952c6f68efb3cf56c39ea325b41af995b
Ceasar15 <ceasarkwadwo@gmail.com>
2021-03-15 17:44:29 +0000

    added static path

--- Kavedal/urls.py
+++ Kavedal/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('home', include('core.urls', namespace='home')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG: 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
