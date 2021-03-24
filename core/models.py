d16c47307ec34bcb93870aeb49f6ef247cd5a169
Ceasar15 <ceasarkwadwo@gmail.com>
2021-03-24 10:47:37 +0000

    added models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactModel(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    email = models.EmailField(null=True)
+    subject = models.TextField()
+    message = models.TextField()
